11a72cc788f895d0299fcb84ba2107e76d1e8103
Ali Hamza Ranjha <hamzaranjha093@gmail.com>
2024-08-01 00:35:04 +0500

    frontend is integrated

---
+++ resources/views/frontend/layout/mainlayout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+@include('frontend.layout.partials.head')
+</head>
+<body>
+@include('frontend.layout.partials.spinner')
+@include('frontend.layout.partials.topbar')
+@include('frontend.layout.partials.nav')
+@include('frontend.layout.partials.header')
+@yield('content')
+@include('frontend.layout.partials.footer')
+@include('frontend.layout.partials.footer-scripts')
+</body>
+</html>
